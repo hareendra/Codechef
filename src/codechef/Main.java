f0f5f0e4720a4fb7a3d584d01b055b76aeac42f7
Hareendra <hareendrareddy@gmail.com>
2013-12-31 20:10:03 +0530

    First commit

---
+++ src/codechef/Main.java
@@ -0,0 +1,16 @@
+package codechef;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reddy
+ * Date: 5/18/13
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static  void main (String[] args)
+    {
+        System.out.println("Hello");
+
+    }
+}
