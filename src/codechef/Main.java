208507f17453db45833993cd42b7db7cc6884dec
Hareendra <hareendrareddy@gmail.com>
2014-01-05 17:19:31 +0530

    Second Commit Working code but failed on codechef

--- src/codechef/Main.java
+++ src/codechef/Main.java
@@ -1,5 +1,8 @@
 package codechef;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: reddy
@@ -8,9 +11,19 @@ package codechef;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
-    public static  void main (String[] args)
+    public static  void main (String[] args) throws Exception
     {
-        System.out.println("Hello");
-        System.out.println("Bye");
+        BufferedReader in   //= new BufferedReader(new InputStreamReader(System.in));
+                = new BufferedReader(new FileReader("in.txt"));
+        int t=Integer.parseInt(in.readLine());
+        for(int j=0;j<t;j++) {
+            String str=in.readLine();
+            if (str.contains("010")
+                    || str.contains("101"))   {
+                System.out.println("Good");
+            }else {
+                System.out.println("Bad");
+            }
+        }
     }
 }
