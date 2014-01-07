70eefe6d6428468e4e01bad041d88b34cb14a714
Hareendra <hareendrareddy@gmail.com>
2014-01-07 19:47:18 +0530

    Bug fixed...j should iterate from 1 instead of 2 Time limit exceeding ..Used mathematical algorithm

--- src/codechef/PLZLYKME.java
+++ src/codechef/PLZLYKME.java
@@ -1,19 +1,16 @@
 package codechef;
-
 import java.io.*;
-import java.util.*;
-
 /**
  * Created by reddy on 1/5/14.
  */
 public class PLZLYKME {
-    public static  void main (String[] args) throws Exception
+    public static void main (String[] args) throws Exception
     {
-        BufferedReader in   //= new BufferedReader(new InputStreamReader(System.in));
-                = new BufferedReader(new FileReader("in.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//= new BufferedReader(new FileReader("in.txt"));
         int t=Integer.parseInt(in.readLine());
 
-        for(int i=0;i<t;i++)    {
+        for(int i=0;i<t;i++) {
             String str = in.readLine();
             String[] strAry=str.split(" ");
 
@@ -22,18 +19,50 @@ public class PLZLYKME {
             int s=Integer.parseInt(strAry[2]);
             int c=Integer.parseInt(strAry[3]);
 
+            /*int pow=power(c+1,d-1,l);
+                for(int j=1;j<d;j++) {
+                    s=(c+1)*s;
+                    if (s>=l)
+                        break;
+            }*/
 
-            for(int j=1;j<d;j++)    {
-                  s=(c+1)*s;
-                  if (s>=l)
-                      break;
-            }
-
-            if (s>=l)    {
+            if (amILikeable(c,d,l,s)) {
                 System.out.println("ALIVE AND KICKING");
-            }else   {
+            }else {
                 System.out.println("DEAD AND ROTTING");
             }
         }
+    }
+
+    static int power(int num, int pow,int likes) {
+        if(pow==0)
+            return 1;
+
+        int hsqr=power(num,pow/2,likes);
+        int sqr=hsqr*hsqr;
+
+        if (pow%2==0) {
+            if (sqr >=likes) {
+                return -1;
+            }
+            return sqr;
+        }else {
+            if (hsqr*num >=likes) {
+                return -1;
+            }
+            return sqr*num;
+        }
+    }
+
+    static boolean amILikeable(double c,double d,double l,double s) {
+        double lhs=(d-1)*Math.log(c+1)+Math.log(s);
+        double rhs=Math.log(l);
+
+        if (Math.abs(lhs-rhs) < 0.00000000001)
+            return true;
+        else if (lhs < rhs)
+            return false;
+        else
+            return true;
     }
 }
