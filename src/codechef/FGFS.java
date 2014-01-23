1d29e17aa58f2c3f88aded64ef61bb3b6443db64
Hareendra <hareendrareddy@gmail.com>
2014-01-23 22:24:38 +0530

    Working with sample input...

--- src/codechef/FGFS.java
+++ src/codechef/FGFS.java
@@ -1,68 +1,87 @@
 package codechef;
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
-/**
- * Created by reddy on 1/7/14.
- */
+class Apartment {
+    private ArrayList<Customer> customers=new ArrayList<Customer>();
+    int id; //Not used ,as array subscript is itself used as id
+
+    public void addCustomer(Customer c) {
+        customers.add(c);
+    }
+    public int calcMaxCustomers() {
+        Collections.sort(customers);
+
+        int max=1;
+
+        Customer last=customers.get(0);
+        for (int i=1;i<customers.size();i++) {
+            if (last.f <= customers.get(i).s) {
+                max++;
+                last=customers.get(i);
+            }
+        }
+        return max;
+    }
+}
+
+class Customer implements Comparable<Customer>{
+    //[s,f)
+    int s; //inclusive
+    int f; //excluding
+
+    public Customer(int s,int f) {
+        this.s=s;
+        this.f=f;
+    }
+    public int compareTo(Customer c) {
+        if(this.f==c.f) {
+            return 0;
+        }else if(this.f<c.f) {
+            return -1;
+        }else {
+            return 1;
+        }
+    }
+}
+
+
 public class FGFS {
     public static void main (String[] args) throws Exception
     {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-//= new BufferedReader(new FileReader("in.txt"));
+//new BufferedReader(new FileReader("in.txt"));
         int t=Integer.parseInt(in.readLine());
 
         for(int i=0;i<t;i++) {
-            int n=Integer.parseInt(in.readLine());
-            int[] ary=new int[n];
+            String[] strAry=in.readLine().split(" ");
+            int n=Integer.parseInt(strAry[0]);
+            int k=Integer.parseInt(strAry[1]);
 
-            String[] strAry= in.readLine().split(" ");
+            Apartment[] apartments=new Apartment[k];
+            for(int j=0;j<k;j++) {
+                apartments[j]=new Apartment();
+            }
+
             for(int j=0;j<n;j++) {
-                ary[j]=Integer.parseInt(strAry[j]);
+                strAry=in.readLine().split(" ");
+                int s=Integer.parseInt(strAry[0]);
+                int f=Integer.parseInt(strAry[1]);
+                int p=Integer.parseInt(strAry[2]);
+
+                apartments[p-1].addCustomer(new Customer(s,f));
             }
 
-            strAry= in.readLine().split(" ");
-
-            int a=Integer.parseInt(strAry[0]);
-            int b=Integer.parseInt(strAry[1]);
-            int c=Integer.parseInt(strAry[2]);
-
-            String s=in.readLine();
-
-            for(int j=0;j<s.length();j++) {
-                if(s.charAt(j) == 'R')
-                    rev(ary,j,n);
-                else if (s.charAt(j) == 'A')
-                    add(ary,j,n,a,c);
-                else if (s.charAt(j) == 'M')
-                    mul(ary, j, n, b, c);
+            int max=0;
+            for(int j=0;j<k;j++) {
+                max+=apartments[j].calcMaxCustomers();
             }
-
-            for(int num:ary) {
-                System.out.print(num+" ");
-            }
-            System.out.println();
-        }
-    }
-    static void rev(int[] ary,int i,int len) {
-        int temp=0;
-        while(i<len) {
-            temp=ary[i];
-            ary[i]=ary[len-i];
-            ary[len-i]=temp;
-            i++;
+            System.out.println(max);
         }
     }
 
-    static void add(int[] ary,int start,int len, int addNum,int modulo) {
-        for(int i=start;i<len;i++) {
-            ary[i]=(ary[i]+addNum)%modulo;
-        }
-    }
-    static void mul(int[] ary,int start,int len, int mulNum,int modulo) {
-        for(int i=start;i<len;i++) {
-            ary[i]=(ary[i]*mulNum)%modulo;
-        }
-    }
 }
